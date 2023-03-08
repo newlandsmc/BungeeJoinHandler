0d016c392c5092358790f1ceeaef6e7357529211
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-03-08 10:41:29 -0500

    don't compile regex every time

--- BungeeJoinMessages/src/main/java/tv/tirco/bungeejoin/util/HexChat.java
+++ BungeeJoinMessages/src/main/java/tv/tirco/bungeejoin/util/HexChat.java
@@ -5,11 +5,15 @@ import net.md_5.bungee.api.ChatColor;
 
 public class HexChat {
 
-	public static final Pattern HEX_PATTERN = Pattern.compile("&#(\\w{5}[0-9a-f])");
+	public static final Pattern HEX_PATTERN = Pattern.compile("&#(\\w{5}[0-9a-f])"),
+	PATTERN_2 = Pattern.compile("(?i)\\&(x|#)([0-9A-F])([0-9A-F])([0-9A-F])([0-9A-F])([0-9A-F])([0-9A-F])");
 	
 	public static String translateHexCodes(String message) {
 		// Translate RGB codes
-		return ChatColor.translateAlternateColorCodes('&', message.replaceAll("(?i)\\&(x|#)([0-9A-F])([0-9A-F])([0-9A-F])([0-9A-F])([0-9A-F])([0-9A-F])", "&x&$2&$3&$4&$5&$6&$7"));
+		//return ChatColor.translateAlternateColorCodes('&', message.replaceAll("(?i)\\&(x|#)([0-9A-F])([0-9A-F])([0-9A-F])([0-9A-F])([0-9A-F])([0-9A-F])", "&x&$2&$3&$4&$5&$6&$7"));
+		String msg = PATTERN_2.matcher(message).replaceAll("&x&$2&$3&$4&$5&$6&$7");
+		// Translate hex codes
+		return ChatColor.translateAlternateColorCodes('&', msg);
 	}
 
 //	public static String translateHexCodes (String textToTranslate) {
